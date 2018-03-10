20d5b5d42c9748bb963b7029dbcf0750cf12661f
Kondal Rao Komaragiri <kondal04@gmail.com>
2018-03-10 08:35:06 -0800

    Adding semantic-ui

--- src/react/app.js
+++ src/react/app.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-const App = () => (
-    <h1>Hello, React!</h1>
-);
+import {Container, Menu} from 'semantic-ui-react';
+
+class App extends React.Component {
+    constructor () {
+        super();
+        this.state = { activeItem: 'home' };
+        this.handleItemClick = this.handleItemClick.bind(this);
+    }
+
+    handleItemClick (e, {name}) {
+        console.log('handleItemClick: ' + name);
+        this.setState({activeItem: name});
+    }
+
+    render () {
+        return (
+            <Container>
+                <Menu secondary fixed='top' >
+                    <Menu.Item header> BRAND </Menu.Item>
+                    <Menu.Item
+                        name='home'
+                        active={this.state.activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                        content='Home' />
+                    <Menu.Menu position='right'>
+                        <Menu.Item
+                            name='about'
+                            active={this.state.activeItem === 'about'}
+                            onClick={this.handleItemClick}
+                            content='About' />
+                    </Menu.Menu>
+                </Menu>
+
+                <Menu secondary text borderless fixed='bottom' size='mini'>
+                    <Menu.Item
+                        content='@kondal.com' />
+                </Menu>
+            </Container>
+        );
+    }
+}
 
 export default App;
--- src/react/index.js
+++ src/react/index.js
@@ -1,14 +1,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
 
-import './styles.scss';
+// import './styles.scss';
+
+import 'semantic-ui-css/semantic.css';
+
+import App from './app';
 
 const rootDiv = document.getElementById('reactapp');
 
 ReactDOM.render(<App />, rootDiv);
 
 if (module.hot) {
+    console.log("Updating the site.");
     module.hot.accept();
 }
